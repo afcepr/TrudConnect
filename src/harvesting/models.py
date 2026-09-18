e89d64ff1a194b8ec84f7439783ab0793d9b76b5
afcepr <afcepr@ya.ru>
2026-09-18 14:48:26 +0300

    Модель языка

--- src/harvesting/models.py
+++ src/harvesting/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название населенного пункта')
-    slug = models.SlugField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название населенного пункта')
+    slug = models.SlugField(max_length=50, blank=True, unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = 'Название населенного пункта'
-        verbose_name_plural = 'Название населенных пунктов'
+        verbose_name_plural = 'Названия населенных пунктов'
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Язык программирования')
+    slug = models.SlugField(max_length=50, blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
