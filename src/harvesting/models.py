49385653a9844a2f3aff1f50d6878f0890aee9be
afcepr <afcepr@ya.ru>
2026-09-18 23:39:37 +0300

    Модель вакансий

--- src/harvesting/models.py
+++ src/harvesting/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from .utils import from_cyrillic_to_eng
+
 
 class City(models.Model):
     name = models.CharField(max_length=50, unique=True, verbose_name='Название населенного пункта')
@@ -7,6 +9,11 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Название населенного пункта'
@@ -20,6 +27,28 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
     class Meta:
         verbose_name = 'Язык программирования'
         verbose_name_plural = 'Языки программирования'
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Инструмент')
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
